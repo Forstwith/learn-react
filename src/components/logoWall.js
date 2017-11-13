4e283cfb0178558cf1d703073bcbdaa16c96105d
forst <xiefuzhanshen@163.com>
2017-11-14 00:19:11 +0800

    complete

---
+++ src/components/logoWall.js
@@ -0,0 +1,31 @@
+
+
+import React from 'react'
+var reactDOM = require('react-dom');
+
+class LogoWallComponent extends React.Component{
+	constructor(props){
+		super(props);
+		this.Constant = {
+
+		};
+		this.state = {
+
+		}
+	}
+
+	componentDidMount(){
+
+	}
+
+	render(){
+		return (<div>helloworld</div>)
+	}
+}
+
+LogoWallComponent.defaultProps = {};
+
+export default LogoWallComponent;
+
+
+
